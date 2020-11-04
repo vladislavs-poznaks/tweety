dd6610bef4f239e8e7437ac6c86f0b29a8998fc4
Vladislavs Pozņaks <vladislavs.poznaks@gmail.com>
2020-11-04 22:31:22 +0200

    Tweet Store validation moved to custom Request Class

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TweetStoreRequest;
 use App\Models\Tweet;
-use Illuminate\Http\Request;
 
 class TweetController extends Controller
 {
@@ -14,15 +14,11 @@ class TweetController extends Controller
         ]);
     }
 
-    public function store(Request $request)
+    public function store(TweetStoreRequest $request)
     {
-        $attributes = $request->validate([
-            'body' => ['required', 'max:255']
-        ]);
-
         Tweet::create([
             'user_id' => auth()->user()->id,
-            'body' => $attributes['body']
+            'body' => $request->body
         ]);
 
         return redirect(route('tweets.index'));
